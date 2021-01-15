5cf3b60d9b327c1be50132100d758e9fbf5b8854
the-mann <marcuspmann@gmail.com>
2021-01-14 23:15:09 -0500

    Initial Commit

---
+++ src/main/java/net/manncraft/industrialfarming/IndustrialFarming.java
@@ -0,0 +1,17 @@
+package net.manncraft.industrialfarming;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class IndustrialFarming extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
